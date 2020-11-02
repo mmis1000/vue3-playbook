228e63d516798dda2e6c0e88494f913efecd99c7
Mmis1000 <mmis1000@yahoo.com.tw>
2020-11-02 17:32:51 +0800

    Use simplified slot syntax

--- src/views/Parallax.vue
+++ src/views/Parallax.vue
@@ -2,8 +2,30 @@
   <div>
     <Parallax style="height: 100vh">
       inline
-      <ParallaxChild style="width: 10%; height: 10%; display: inline-block; vertical-align: middle">
-        <template v-slot:default="{ height, scroll }">
+      <ParallaxChild
+        style="width: 10%; height: 10%; display: inline-block; vertical-align: middle"
+        v-slot="{ height, scroll }"
+      >
+        <div
+          :style="{
+            height: height + 'px',
+            background:'url(https://i.imgur.com/HKyMBF1.jpg)',
+            backgroundSize: '100% auto',
+            backgroundPosition: `${-scroll / 2}px ${scroll / 2}px`,
+            willChange: 'background-position',
+            color: 'blue' 
+          }"
+        >hhh<br>hhh<br>hhh<br>hhh<br>hhh<br></div>
+      </ParallaxChild>
+      Do work
+
+      <br>
+
+      <template v-for="index in 100" :key="index">
+        <ParallaxChild
+          style="height: calc(100% / 3); width: calc(100% / 3); display: inline-block; vertical-align: middle"
+          v-slot="{ height, scroll }"
+        >
           <div
             :style="{
               height: height + 'px',
@@ -14,39 +36,20 @@
               color: 'blue' 
             }"
           >hhh<br>hhh<br>hhh<br>hhh<br>hhh<br></div>
-        </template>
-      </ParallaxChild>
-      Do work
-
-      <br>
-
-      <template v-for="index in 100" :key="index">
-        <ParallaxChild style="height: calc(100% / 3); width: calc(100% / 3); display: inline-block; vertical-align: middle">
-          <template v-slot:default="{ height, scroll }">
-            <div
-              :style="{
-                height: height + 'px',
-                background:'url(https://i.imgur.com/HKyMBF1.jpg)',
-                backgroundSize: '100% auto',
-                backgroundPosition: `${-scroll / 2}px ${scroll / 2}px`,
-                willChange: 'background-position',
-                color: 'blue' 
-              }"
-            >hhh<br>hhh<br>hhh<br>hhh<br>hhh<br></div>
-          </template>
         </ParallaxChild>
-        <ParallaxChild style="height: calc(100% / 3); width: calc(100% / 3); display: inline-block; vertical-align: middle">
-          <template v-slot:default="{ height, scroll }">
-            <div
-              :style="{
-                height: height + 'px',
-                background: 'url(https://i.imgur.com/t9XlvvL.jpg)',
-                backgroundSize: '100% auto',
-                backgroundPosition: `${scroll / 2}px ${scroll / 2}px`,
-                willChange: 'background-position',
-              }"
-            >aaa<br>aaa<br>aaa<br>aaa<br>aaa<br></div>
-          </template>
+        <ParallaxChild
+          style="height: calc(100% / 3); width: calc(100% / 3); display: inline-block; vertical-align: middle"
+          v-slot="{ height, scroll }"
+        >
+          <div
+            :style="{
+              height: height + 'px',
+              background: 'url(https://i.imgur.com/t9XlvvL.jpg)',
+              backgroundSize: '100% auto',
+              backgroundPosition: `${scroll / 2}px ${scroll / 2}px`,
+              willChange: 'background-position',
+            }"
+          >aaa<br>aaa<br>aaa<br>aaa<br>aaa<br></div>
         </ParallaxChild>
         <h1 v-if="index === 50">你沒看到我(X</h1>
       </template>
